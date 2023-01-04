ac7e2634fcbca192ade0a52f5096efb6c1d33c56
Akash Thakur <117536529+akashgppp@users.noreply.github.com>
2023-01-04 14:06:35 +0530

    Update nucount.java

--- Task5/nucount.java
+++ Task5/nucount.java
@@ -4,11 +4,12 @@ public static void main(String[] rgs){
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter the Value");
 int num= sc.nextInt();
+int org_n=num;
 int count=0;
 while(num>0){
 num=num/10;
 count=count+1;
 }
-System.out.println("number of count"+num);
+System.out.println("number of count.."+org_n+"="+count);
 }
 }
